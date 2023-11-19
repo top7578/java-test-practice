7b26c6c7a78cfd3233eaa394158c28852165a925
Kwon JunWoo <top7578@hanwha.com>
2023-11-19 21:34:40 +0900

    feat: init

---
+++ src/main/java/com/example/sample/SampleApplication.java
@@ -0,0 +1,34 @@
+package com.example.sample;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SampleApplication {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String result = scanner.nextLine();
+        String[] parts = result.split(" ");
+        long num1 = Long.parseLong(parts[0]);
+        long num2 = Long.parseLong(parts[2]);
+        String operator = parts[1];
+        long answer = 0;
+        switch(operator) {
+            case "+":
+                answer = num1 + num2;
+                break;
+            case "-":
+                answer = num1 - num2;
+                break;
+            case "*":
+                answer = num1 * num2;
+                break;
+            case "/":
+                answer = num1 / num2;
+                break;
+            default:
+                throw new InvalidOperatorException();
+        }
+        System.out.println(answer);
+    }
+}
